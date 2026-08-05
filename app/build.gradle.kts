08faf836abc466dc8c487dd123960a33bb896842
Mika <mikasmartin@gmail.com>
2026-08-05 12:34:28 -0700

    chore: bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.secondmonday.earnit"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "com.earnit.app.HiltTestRunner"
     }
